3ad788c22b77c4d64853abe57e0a0e6ca09a2ef3
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-01-11 19:01:07 +0000

    console.py --contains bugs

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """console.py"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,17 +12,50 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def help_quit(self):
-        print('\n'.join(['quit',
-                          'To quit the program',
-                          ]))
+        print('\n'.join(['Quit command to exit the program']))
+
+    def do_create(self, arg):
+        if arg:
+            if arg == BaseModel.__name__:
+                new_instance = BaseModel()
+                new_instance.save() 
+                print(new_instance.id)
+
+            if arg != BaseModel.__name__:
+                print(f"** class doesn't exist **")
+
+        else:
+            print(f"** class name missing **")
+
+    def help_create(self):
+        print('\n'.join(['Creates a new instance of BaseModel',
+                         'saves it (to the JSON file) and prints the id.',
+                         ]))
+
+    def do_show(self, *args):
+        n = len(args)
+        if args:
+            if n > 0 and args[0] == BaseModel.__name__:
+                new = BaseModel()
+                if n == 1:
+                    print("** instance id missing **")
+                elif n == 2 and args[1] != new.id:
+                    print("** no instance found **")
+                elif n == 2 and args[1] == new.id:
+                    print(str(new))  # Corrected: Call __str__ method
+            elif n > 0 and args[0] != BaseModel.__name__:
+                print("** class doesn't exist **")
+        elif not args:
+            print("** class name missing **")
+    
+    def help_show(self):
+        print('\n'.join(['Prints the string representation',
+                         'of an instance based on the class name and id.',
+                         ]))
 
     def do_EOF(self, line):
         """EOF command to exit the program"""
         return True
-
-    def do_create(self):
-        pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -47,7 +47,7 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save(self)
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of
