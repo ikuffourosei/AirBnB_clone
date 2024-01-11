9d7037b1515a71e4f88ae7a254eec7bd21e411d5
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-01-11 15:03:38 +0000

    console --update

--- console.py
+++ console.py
@@ -2,23 +2,26 @@
 """console.py"""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
         return True
 
-    def do_EOF(self):
+    def help_quit(self):
+        print('\n'.join(['quit',
+                          'To quit the program',
+                          ]))
+
+    def do_EOF(self, line):
         """EOF command to exit the program"""
         return True
 
-    def do_create(self, arg):
-        """Creates a new instance of BaseModel"""
+    def do_create(self):
         pass
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
