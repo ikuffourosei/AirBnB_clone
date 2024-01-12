81ef5b9d3ea2350f4a47b243c0eb95d5d27d3486
ProvLeon <providence.leonard@gmail.com>
2024-01-12 06:19:20 +0000

    updated

--- console.py
+++ console.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """console.py"""
 import cmd
+import sys
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
     prompt = '(hbnb) '
+
+    models = {"BaseModel", "User", "State", "City", "Amenity", "Place", "Review"}
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -16,7 +20,13 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Creates a new instance of BaseModel"""
-        pass
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.models:
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            print(new_instance.id)
 
 
 if __name__ == '__main__':
