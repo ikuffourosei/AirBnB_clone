975198f4b0ac39dc9ea2304eca6b7cb5b4811426
ProvLeon <providence.leonard@gmail.com>
2024-01-12 09:29:14 +0000

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
 
         if kwargs:
