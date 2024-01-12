5f6a67855dbdfcadd0d89650ce0a134b415306cd
ProvLeon <providence.leonard@gmail.com>
2024-01-12 09:25:15 +0000

    created a tests branch to implement the unittest for each class and function for the project

--- models/base_model.py
+++ models/base_model.py
@@ -20,10 +20,11 @@ class BaseModel:
             *args: list containing positional arguments
             **kwargs: dictionary containing key/value arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
         if kwargs:
             kwargs.pop("__class__")
@@ -36,8 +37,6 @@ class BaseModel:
             if "updated_at" in kwargs:
                 value = datetime.strptime(kwargs["updated_at"], format)
                 self.updated_at = value
-        if not kwargs:
-            storage.new(self)
 
     def __str__(self):
         """Return the print() and str() representation of the BaseModel"""
