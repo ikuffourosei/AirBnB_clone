3fc340a044ff16306c1c7f0458d59409c0c36c58
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-01-14 19:51:23 +0000

    all files and tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -32,7 +32,7 @@ class TestBaseModel(unittest.TestCase):
         if os.path.isfile(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
-    def test_3_instantiation(self):
+    def test_BaseModel_instantiation(self):
         """Tests instantiation of BaseModel class."""
 
         b = BaseModel()
@@ -54,15 +54,6 @@ class TestBaseModel(unittest.TestCase):
         args = [i for i in range(1000)]
         b = BaseModel(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         b = BaseModel(*args)
-
-    def test_BaseModel_attributes(self):
-        """Tests attributes value for instance of a BaseModel class."""
-
-        attributes = storage.attributes()["BaseModel"]
-        o = BaseModel()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
 
     def test_BaseModel_datetime_created(self):
         """Tests if updated_at & created_at are current at creation."""
@@ -135,30 +126,7 @@ class TestBaseModel(unittest.TestCase):
         msg = "to_dict() takes 1 positional argument but 2 were given"
         self.assertEqual(str(e.exception), msg)
 
-    def test_BaseModel_instantiation(self):
-        """Tests instantiation with **kwargs."""
-
-        my_model = BaseModel()
-        my_model.name = "Holberton"
-        my_model.my_number = 89
-        my_model_json = my_model.to_dict()
-        my_new_model = BaseModel(**my_model_json)
-        self.assertEqual(my_new_model.to_dict(), my_model.to_dict())
-
-    def test_BaseModel_instantiation_dict(self):
-        """Tests instantiation with **kwargs from custom dict."""
-        d = {"__class__": "BaseModel",
-             "updated_at":
-             datetime(2050, 12, 30, 23, 59, 59, 123456).isoformat(),
-             "created_at": datetime.now().isoformat(),
-             "id": uuid.uuid4(),
-             "var": "foobar",
-             "int": 108,
-             "float": 3.14}
-        o = BaseModel(**d)
-        self.assertEqual(o.to_dict(), d)
-
-    def test_5_save(self):
+    def test_BaseModel_save(self):
         """Tests that storage.save() is called from save()."""
         self.resetStorage()
         b = BaseModel()
@@ -172,7 +140,7 @@ class TestBaseModel(unittest.TestCase):
             f.seek(0)
             self.assertEqual(json.load(f), d)
 
-    def test_5_save_no_args(self):
+    def test_BaseModel_save_no_args(self):
         """Tests save() with no arguments."""
         self.resetStorage()
         with self.assertRaises(TypeError) as e:
@@ -180,7 +148,7 @@ class TestBaseModel(unittest.TestCase):
         msg = "save() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), msg)
 
-    def test_5_save_excess_args(self):
+    def test_BaseModel_save_excess_args(self):
         """Tests save() with too many arguments."""
         self.resetStorage()
         with self.assertRaises(TypeError) as e:
